0a6731e60e5b1ce8837584d52babea336c6d5e85
Hugo Aka Sawsyn <thedarklinx@hotmail.fr>
2021-11-02 11:13:47 +0100

    initial push

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '460b5523d266c441a1430f87b7485575'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '16085610-6f4a-45b9-8ed9-860aa416b7f1'
+};
